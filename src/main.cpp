2d8ac5fee0323fa0397a1d339ea81fb6bcdbb22a
michalshy <91937056+michalshy@users.noreply.github.com>
2024-06-08 13:30:07 +0200

    Basic textEditor project

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,30 @@
 //
 
 
-int main()
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+
+#include "MainWindow.hpp"
+
+int main(int argc, char *argv[])
 {
-    return 0;
+    Q_INIT_RESOURCE(application);
+
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setApplicationName("Application Example");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::applicationName());
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("file", "The file to open.");
+    parser.process(app);
+
+    MainWindow mainWin;
+    if (!parser.positionalArguments().isEmpty())
+        mainWin.loadFile(parser.positionalArguments().first());
+    mainWin.show();
+    return app.exec();
 }
